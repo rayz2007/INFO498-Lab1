795e052e4530a85824d071b35d88c5042c83dcb4
rayz2007 <rayz2007@uw.edu>
2019-04-03 11:50:37 -0700

    complete

--- exercise/problem-a.ts
+++ exercise/problem-a.ts
@@ -1,7 +1,16 @@
 // Given an array of ints a and an int k, return if there exists a pair (x, y) in a such that x + y = k.
 // This can be solved in O(nlog(n)) time trivially, or even in O(n) if you use another data structure
 function hasPairSumToK(a: number[], k: number): boolean {
-    
+
+ 
+    var arr : { [key:number]:number; } = {};
+    for (let i = 0; i < a.length; i++) {
+       if (arr[k - a[i]] != undefined) {
+           return true;
+       }
+       arr[a[i]] = i;
+    }
+    return false;
 }
 
 
